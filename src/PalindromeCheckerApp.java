fcbb01a5e619b4f166b96b72005c24f191f5824e
Primordius2007 <narayanparasher007@gmail.com>
2026-03-09 15:38:38 +0530

    UC12: ADVANCED STRATEGY

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -184,3 +184,25 @@ class PalindromeService {
         return true;
     }
 }
+
+interface PalindromeStrategy {
+    boolean check(String input);
+}
+
+class StackStrategy implements PalindromeStrategy {
+
+    public boolean check(String input) {
+        java.util.Stack<Character> stack = new java.util.Stack<>();
+
+        for (char c : input.toCharArray()) {
+            stack.push(c);
+        }
+
+        for (char c : input.toCharArray()) {
+            if (c != stack.pop()) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
